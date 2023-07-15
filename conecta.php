2460ebc40ce0dca3adcb9fd8f8b41a759e102c1a
IgorCRH <125159948+IgorCRH@users.noreply.github.com>
2023-07-15 19:51:11 -0300

    Update conecta.php

--- conecta.php
+++ conecta.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "nomedousuarioaqui";
-$password = "";
-$dbname = "avaliacaodedocentes";
+$username = "nomedousuariodamaquinaaqui";
+$password = "setiversenhanamaquinacoloqueaqui";
+$dbname = "nomedadatabasenophpmyadminaqui";
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
